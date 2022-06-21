d110eec2af1e550cab428f47fa49fc0ee1d3e98f
Suellen Priscilla de Camargo <suellen.priscilla.camargo@gmail.com>
2022-06-21 00:11:47 -0300

    feat: add some configrations to deploy to heroku

--- src/db/context.ts
+++ src/db/context.ts
@@ -2,14 +2,27 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import Role from '../models/Role';
 import User from '../models/User';
+import { herokuDBData } from '../utils/getHerokuDBData';
+
+// postgres://{user}:{password}@{hostname}:{port}/{database-name}
+const DATABASE_URL_LOCAL =
+  'postgres://postgres:postgres@localhost:5432/e-commerce-api';
+
+const dbObject = herokuDBData(
+  process.env.NODE_ENV === 'production'
+    ? process.env.DATABASE_URL
+    : DATABASE_URL_LOCAL,
+);
+
+const { host, port, username, password, database } = dbObject;
 
 const AppDataSource = new DataSource({
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'postgres',
-  database: 'e-commerce-api',
+  host,
+  port,
+  username,
+  password,
+  database,
   synchronize: false,
   logging: false,
   entities: [Role, User],
